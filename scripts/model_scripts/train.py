7a6626c865e0e46eb279752a4c1eae21b4fbcad3
Ch00cha <anton.vas2k@gmail.com>
2023-11-06 02:30:23 +0500

    update

--- scripts/model_scripts/train.py
+++ scripts/model_scripts/train.py
@@ -6,7 +6,6 @@ import pickle
 
 params_train = yaml.safe_load(open("params.yaml"))["train"]
 train_data = pd.read_csv('./data/train.csv')
-print(train_data)
 y_train = train_data['smoke']
 X_train = train_data.drop('smoke', axis = 1)
 clf = RandomForestClassifier(max_depth = params_train["max_depth"],
